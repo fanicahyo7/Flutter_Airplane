3edc44baedc6dec051e655ac7e123880fef749dd
fanidc7 <fanidc7@gmail.com>
2021-08-02 10:44:51 +0700

    first commit & theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor =  Color(0xff5C40CC);
+Color kWhiteColor = Color(0xffFFFFFF);
+Color kBlackColor = Color(0xff1F1449);
+Color kGreyColor = Color(0xff9698A9);
+Color kGreenColor = Color(0xff0EC3AE);
+Color kRedColor = Color(0xffEB70A5);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: kPrimaryColor
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor
+);
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: kGreenColor
+);
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: kRedColor
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extrabold = FontWeight.w800;
+FontWeight black = FontWeight.w900;

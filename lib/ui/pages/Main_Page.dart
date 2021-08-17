c5a4ddb6c07857be2bf4cccba49bdc4d601d2521
fanicahyo7 <fanidc7@gmail.com>
2021-08-17 12:03:15 +0700

    main page dan bottom navigation state index

--- lib/ui/pages/Main_Page.dart
+++ lib/ui/pages/Main_Page.dart
@@ -1,7 +1,12 @@
+import 'package:bwa_airplane/cubit/page_cubit.dart';
 import 'package:bwa_airplane/shared/theme.dart';
 import 'package:bwa_airplane/ui/pages/Home_Page.dart';
+import 'package:bwa_airplane/ui/pages/Setting_Page.dart';
+import 'package:bwa_airplane/ui/pages/Transaction_Page.dart';
+import 'package:bwa_airplane/ui/pages/Wallet_Page.dart';
 import 'package:bwa_airplane/ui/widgets/custom_bottom_navigation_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -22,29 +27,54 @@ class MainPage extends StatelessWidget {
             children: [
               CustomBottomNavigationItem(
                 imageUrl: 'assets/icon_home.png',
-                isSelected: true,
+                index: 0,
               ),
-              CustomBottomNavigationItem(imageUrl: 'assets/icon_booking.png'),
-              CustomBottomNavigationItem(imageUrl: 'assets/icon_card.png'),
-              CustomBottomNavigationItem(imageUrl: 'assets/icon_settings.png'),
+              CustomBottomNavigationItem(
+                imageUrl: 'assets/icon_booking.png',
+                index: 1,
+              ),
+              CustomBottomNavigationItem(
+                imageUrl: 'assets/icon_card.png',
+                index: 2,
+              ),
+              CustomBottomNavigationItem(
+                imageUrl: 'assets/icon_settings.png',
+                index: 3,
+              ),
             ],
           ),
         ),
       );
     }
 
-    Widget homepage() {
-      return HomePage();
+    Widget page(int currentIndex) {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+        case 1:
+          return TransactionPage();
+        case 2:
+          return WalletPage();
+        case 3:
+          return SettingPage();
+
+        default:
+          return HomePage();
+      }
     }
 
-    return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: Stack(
-        children: [
-          homepage(),
-          customBottomNavigation(),
-        ],
-      ),
+    return BlocBuilder<PageCubit, int>(
+      builder: (context, currentIndex) {
+        return Scaffold(
+          backgroundColor: kBackgroundColor,
+          body: Stack(
+            children: [
+              page(context.read<PageCubit>().state),
+              customBottomNavigation(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
